f548970eafbe95cfa0a95a4378734bba3b9d2005
ZeppelinGames <31507417+ZeppelinGames@users.noreply.github.com>
2020-02-01 14:14:39 +1100

    Initial commit

---
+++ Assets/Scripts/Build.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Build : MonoBehaviour
+{
+    public GameObject[] buildObjects;
+    private int currObject;
+
+    [Space(10)]
+    public Text objectName;
+
+    private List<Vector3> pos = new List<Vector3>();
+    private List<int> ids = new List<int>();
+
+    public static List<Vector3> staticPos = new List<Vector3>();
+    public static List<int> staticIds = new List<int>();
+
+    void Update()
+    {
+        BuildBlock();
+        DestroyBlock();
+        ChangeObject();
+
+        staticPos = pos;
+        staticIds = ids;
+    }
+
+    void ChangeObject()
+    {
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            currObject++;
+        }
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            currObject--;
+        }
+
+        currObject = Mathf.Clamp(currObject, 0, buildObjects.Length - 1);
+
+        objectName.text = buildObjects[currObject].name;
+    }
+
+    void BuildBlock()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 10))
+            {
+                if (hit.transform != transform)
+                {
+                    int x = Mathf.RoundToInt(hit.point.x);
+                    int y = Mathf.RoundToInt(hit.point.y);
+                    int z = Mathf.RoundToInt(hit.point.z);
+
+                    Vector3 tempPos = new Vector3(x, y, z);
+
+                    if (!pos.Contains(tempPos))
+                    {
+                        int rotX = Mathf.RoundToInt(transform.eulerAngles.x / 90) * 90;
+                        int rotY = Mathf.RoundToInt(transform.eulerAngles.y / 90) * 90;
+                        int rotZ = Mathf.RoundToInt(transform.eulerAngles.z / 90) * 90;
+
+                        GameObject newBlock = Instantiate(buildObjects[currObject], new Vector3(x, y, z), Quaternion.Euler(new Vector3(rotX,rotY,rotZ)));                    
+
+                        newBlock.transform.eulerAngles = new Vector3(rotX, rotY, rotZ);
+
+                        pos.Add(tempPos);
+                        ids.Add(currObject + 1);
+                    }
+                }
+            }
+        }
+    }
+
+    void DestroyBlock()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 10))
+            {
+                if (pos.Contains(hit.transform.position))
+                {
+                    int index = pos.IndexOf(hit.transform.position);
+                    pos.RemoveAt(index);
+                    ids.RemoveAt(index);
+
+                    Destroy(hit.transform.gameObject);
+                }
+            }
+        }
+    }
+
+    public static int GetBlockID(Vector3 getPosition)
+    {
+        if (staticPos.Contains(getPosition))
+        {
+            int index = staticPos.IndexOf(getPosition);
+            return staticIds[index];
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Power.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Power : MonoBehaviour
+{
+    public bool isSource;
+    public bool powered;
+    public int powerDelay=0;
+
+    private bool delayed;
+
+    [Space(10)]
+    public Transform reciever;
+
+    [Space(10)]
+    public List<Vector3> inPositions = new List<Vector3>();
+    public List<Vector3> outPositions = new List<Vector3>();
+
+    [Space(10)]
+    public Color poweredColor;
+    public Color unpoweredColor;
+
+    private void Start()
+    {
+        AdjustInOutputs();
+        if (powerDelay > 0)
+        {
+            StartCoroutine(PoweredDelay());
+        }
+    }
+
+    private void Update()
+    {
+        CheckAdjacent();
+
+        if (reciever == null)
+        {
+            if (!isSource)
+            {
+                if (powerDelay <= 0)
+                {
+                    powered = false;
+                    GetComponent<MeshRenderer>().material.color = unpoweredColor;
+                }
+            }
+        }
+        else
+        {
+            if (powerDelay <= 0)
+            {
+                powered = true;
+                GetComponent<MeshRenderer>().material.color = poweredColor;
+            }
+        }
+    }
+
+    IEnumerator PoweredDelay()
+    {
+        while (powered)
+        {
+            GetComponent<MeshRenderer>().material.color = poweredColor;
+            yield return new WaitForSeconds(powerDelay);
+            powered = true;
+            powered = false;
+            GetComponent<MeshRenderer>().material.color = unpoweredColor;
+            yield return new WaitForSeconds(powerDelay);
+            powered = true;
+        }
+    }
+
+    void AdjustInOutputs()
+    {
+        List<Vector3> newInPos = new List<Vector3>();
+        foreach (Vector3 pos in inPositions)
+        {
+            newInPos.Add(transform.position + transform.TransformDirection(pos));
+        }
+
+        inPositions = newInPos;
+
+        List<Vector3> newOutPos = new List<Vector3>();
+        foreach (Vector3 pos in outPositions)
+        {
+            newOutPos.Add(transform.position + transform.TransformDirection(pos));
+        }
+
+        outPositions = newOutPos;
+    }
+
+    void CheckAdjacent()
+    {
+        List<Transform> nearby = new List<Transform>();
+        foreach (Vector3 pos in inPositions)
+        {
+            Collider[] cols = Physics.OverlapBox(pos, new Vector3(0.25f, 0.25f, 0.25f));
+            foreach (Collider col in cols)
+            {
+                if (col.transform != transform)
+                {
+                    if (col.GetComponent<Power>())
+                    {
+                        nearby.Add(col.transform);
+                    }
+                }
+            }
+        }
+
+        Transform decidedReciever = null;
+        foreach (Transform near in nearby)
+        {
+            bool correctPos = false;
+            if (!near.GetComponent<Power>().isSource)
+            {
+                foreach (Vector3 pos in near.GetComponent<Power>().outPositions)
+                {
+                    if (transform.position == pos)
+                    {
+                        correctPos = true;
+                    }
+                }
+            }
+            else
+            {
+                correctPos = true;
+            }
+
+            if (correctPos)
+            {
+                if (near.GetComponent<Power>().reciever != transform)
+                {
+                    if (near.GetComponent<Power>().powered)
+                    {
+                        decidedReciever = near;
+                    }
+                }
+            }
+        }
+
+        reciever = decidedReciever;
+    }
+}
